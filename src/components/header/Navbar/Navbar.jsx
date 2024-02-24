0b48b4aaf01b3b19f5ecd689fc3adf3b5a079572
song-richard <rsong1993@gmail.com>
2024-02-23 17:01:05 -0800

    Added routes to Navbar

--- src/components/header/Navbar/Navbar.jsx
+++ src/components/header/Navbar/Navbar.jsx
@@ -3,10 +3,19 @@ import { BrowserRouter, Routes, Route, Link }  from 'react-router-dom'
 export default function Navbar() {
     return (
         <BrowserRouter>
-            <Link to='/about'>About Me</Link>
-            <Link to='/portfolio'>Portfolio</Link>
-            <Link to='/contact'>Contact</Link>
-            <Link to='/resume'>Resume</Link>
+            <nav>
+                <Link to='/about'>About Me</Link>
+                <Link to='/portfolio'>Portfolio</Link>
+                <Link to='/contact'>Contact</Link>
+                <Link to='/resume'>Resume</Link>
+            </nav>
+
+            <Routes>
+                <Route path='/about' element={<About />} />
+                <Route path='/portfolio'/>
+                <Route path='/contact'/>
+                <Route path='/resume'/>
+            </Routes>
         </BrowserRouter>
     )
 }
