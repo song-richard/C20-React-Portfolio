c7913f840df684f5bd4a6a93fe106e07418c928e
song-richard <rsong1993@gmail.com>
2024-02-23 17:39:00 -0800

    Added form logic in Contact.jsx. Added sample handleSubmit logic to form

--- src/components/header/Navbar/NavbarItems/Contact.jsx
+++ src/components/header/Navbar/NavbarItems/Contact.jsx
@@ -1,15 +1,38 @@
 import { useState } from 'react';
 
 export default function Contact() {
-    const [form, setForm] = useState('');
+    //Variables for Name, Email, and Message
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    //Form Logic
+    const handleForm = (e) => {
+        e.preventDefault();
+
+        const { name, value } = e.target;
+
+        if (name === 'name') {
+            setName(value);
+        } else if (name === 'email') {
+            setEmail(value);
+        } else {
+            setMessage(value);
+        };
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Form Submitted! Name: ${name}, Email: ${email}, Message: ${message}`);
+    };
 
     return (
         <>
             <h1>Contact Page</h1>
-            <form>
-                <input name='name' placeholder='Name'/>
-                <input name='email' placeholder='Email'/>
-                <input name='message' placeholder='Message'/>
+            <form onSubmit={handleSubmit}>
+                <input name='name' value={name} onChange={handleForm} placeholder='Name'/>
+                <input name='email' value={email} onChange={handleForm} placeholder='Email'/>
+                <input name='message' value={message} onChange={handleForm} placeholder='Message'/>
                 <button>Submit</button>
             </form>
         </>
