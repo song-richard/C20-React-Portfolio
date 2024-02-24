40885ef1748ae15e1c0490b81b625e22d305973d
song-richard <rsong1993@gmail.com>
2024-02-23 16:52:21 -0800

    Added Navbar links to Navbar.jsx. Imported react-router-dom

---
+++ src/components/header/Navbar/AboutMe.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route }  from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <>
+            <Link>About Me</Link>
+            <Link>Portfolio</Link>
+            <Link>Contact</Link>
+            <Link>Resume</Link>
+        </>
+    )
+}
+
+export default Navbar;
