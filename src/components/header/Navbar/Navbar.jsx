2d9aceefec9fe481eee0010bfb87f27993e7a6e9
song-richard <rsong1993@gmail.com>
2024-02-24 13:33:35 -0800

    Added Material UI imports, rendered Appbar component

--- src/components/header/Navbar/Navbar.jsx
+++ src/components/header/Navbar/Navbar.jsx
@@ -6,6 +6,21 @@ import {
 }
 from 'react-router-dom';
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+
 import About from './NavbarItems/About';
 import Portfolio from './NavbarItems/Portfolio';
 import Contact from './NavbarItems/Contact';
@@ -13,21 +28,29 @@ import Resume from './NavbarItems/Resume';
 
 export default function Navbar() {
     return (
-        <BrowserRouter>
-            <nav>
-                <Link to='/about'>About Me</Link>
-                <Link to='/portfolio'>Portfolio</Link>
-                <Link to='/contact'>Contact</Link>
-                <Link to='/resume'>Resume</Link>
-            </nav>
+        <>
+            <AppBar>
+                <Container>
+                    <BrowserRouter>
+                        <nav>
+                            <Link to='/about'>About Me</Link>
+                            <Link to='/portfolio'>Portfolio</Link>
+                            <Link to='/contact'>Contact</Link>
+                            <Link to='/resume'>Resume</Link>
+                        </nav>
 
-            <Routes>
-                <Route path='/about' element={<About />} />
-                <Route path='/portfolio' element={<Portfolio />}/>
-                <Route path='/contact' element={<Contact />}/>
-                <Route path='/resume' element={<Resume />}/>
-            </Routes>
-        </BrowserRouter>
+                        <Routes>
+                            <Route path='/about' element={<About />} />
+                            <Route path='/portfolio' element={<Portfolio />}/>
+                            <Route path='/contact' element={<Contact />}/>
+                            <Route path='/resume' element={<Resume />}/>
+                        </Routes>
+                    </BrowserRouter>
+                </Container>
+            </AppBar>
+        </>
+
+
     );
 };
 
