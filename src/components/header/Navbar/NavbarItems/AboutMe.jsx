f8cfd9bc1ffae0854e0839e48456032a7306538b
song-richard <rsong1993@gmail.com>
2024-02-25 20:55:21 -0800

    Updated AboutMe to objects

--- src/components/header/Navbar/NavbarItems/AboutMe.jsx
+++ src/components/header/Navbar/NavbarItems/AboutMe.jsx
@@ -2,25 +2,32 @@ import { Box } from "@mui/system";
 
 export default function About() {
 
-    const bio = 'Hi! Im Richard Song, a Full-Stack Software Developer!';
-    const frontEndTech = 'React.js, Material UI, Tailwind CSS, Bulma, Bootstrap';
-    const backEndTech = 'Express.js, Node.js, MongoDB, mySQL, Firebase, AuthO, Bcrypt, Axios, GraphQL';
+    const richard = [
+        {
+            frontendTitle: 'Front-End Technologies:',
+            backendTitle: 'Back-End Technologies:'
+        },
+        {
+            bio: 'Hi! Im Richard Song, a Full-Stack Software Developer!',
+            frontend:'React.js, Material UI, Tailwind CSS, Bulma, Bootstrap',
+            backend:'Express.js, Node.js, MongoDB, mySQL, Firebase, AuthO, Bcrypt, Axios, GraphQL',
+        }
+    ]
 
     return (
         <>
-            <h1>{bio}</h1>
+            <h1>{richard.bio}</h1>
 
             <Box>
                 <Box>Skills: 
-
                     <Box>
-                        <h3>Front End Technologies:</h3>
-                        <h4>{frontEndTech}</h4>
+                        <h3>{richard[0].frontendTitle}:</h3>
+                        <h4>{richard[1].frontend}</h4>
                     </Box>
-
+                    <br></br>
                     <Box>
-                        <h3>Back-End Technologies</h3>
-                        <h4>{backEndTech}</h4>
+                        <h3>{richard[0].backendTitle}</h3>
+                        <h4>{richard[1].backend}</h4>
                     </Box>
 
                 </Box>
