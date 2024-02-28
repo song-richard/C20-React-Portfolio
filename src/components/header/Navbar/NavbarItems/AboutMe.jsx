fa3d3cc4418ada6c3b7142d7fca7e9b8183b8b4f
song-richard <rsong1993@gmail.com>
2024-02-27 17:25:02 -0800

    Added tailwind styling to AboutMe

--- src/components/header/Navbar/NavbarItems/AboutMe.jsx
+++ src/components/header/Navbar/NavbarItems/AboutMe.jsx
@@ -19,22 +19,23 @@ export default function About() {
 
     return (
         <>
-            <h1>{richard.bio}</h1>
+            <div className="mx-auto max-w-lg text-center">
+                <h1 className="text-3xl font-bold my-8">{richard[2].bio}</h1>
 
-            <Box>
-                <Box>{richard[0].skillsTitle} 
-                    <Box>
-                        <h3>{richard[1].frontendTitle}:</h3>
-                        <h4>{richard[2].frontend}</h4>
-                    </Box>
-                    <br></br>
-                    <Box>
-                        <h3>{richard[1].backendTitle}</h3>
-                        <h4>{richard[2].backend}</h4>
-                    </Box>
-
-                </Box>
-            </Box>
+                <div className="bg-gray-100 rounded-lg p-8">
+                    <h2 className="text-lg font-bold mb-4">{richard[0].skillsTitle}</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div>
+                            <h3 className="text-xl font-bold mb-2">{richard[1].frontendTitle}</h3>
+                            <p className="text-base">{richard[2].frontend}</p>
+                        </div>
+                        <div>
+                            <h3 className="text-xl font-bold mb-2">{richard[1].backendTitle}</h3>
+                            <p className="text-base">{richard[2].backend}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
