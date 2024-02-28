59b8da1b27a24f54f2c011bd5c0309946db82f24
song-richard <rsong1993@gmail.com>
2024-02-27 17:54:10 -0800

    Added resume link to under Resume navigation

--- src/components/header/Navbar/NavbarItems/Resume.jsx
+++ src/components/header/Navbar/NavbarItems/Resume.jsx
@@ -1,7 +1,14 @@
 export default function Resume() {
     return (
-        <>
-            <h1>Resume Page</h1>
-        </>
+        <div className="max-w-md mx-auto py-8 text-center">
+            <a 
+                href="https://www.richardsong.dev/assets/RICHARD%20SONG%20-%20RESUME.pdf"
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                View Resume
+            </a>
+        </div>
     )
 }
